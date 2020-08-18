310998a4881cf6f11904bea2d34040fde116e892
ROBERT ANGEL TORRES <rtorres61595@gmail.com>
2020-08-18 19:54:32 -0400

    for ren

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SkateMap from './components/pages/SkateMap';
+import Spots from './components/pages/Spots';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/skate-map' component={SkateMap} />
+          <Route path='/products' component={Spots} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/pages/SkateMap.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function SkateMap() {
+  return <h1 className='skate-map'>SkateMap</h1>;
+}
